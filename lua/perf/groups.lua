c3b58c26d55ca79e3f6b93868044e08876096e8a
Reilly Spitzfaden <reillypascal@gmail.com>
2025-12-05 15:24:04 -0500

    switched info/hint colors

--- lua/perf/groups.lua
+++ lua/perf/groups.lua
@@ -290,8 +290,8 @@ M.setup = function()
 		--
 		LspDiagnosticsDefaultError = { fg = colors.salmon_pink },
 		LspDiagnosticsDefaultWarning = { fg = colors.dusky_yellow },
-		LspDiagnosticsDefaultInformation = { fg = colors.robin_blue },
-		LspDiagnosticsDefaultHint = { fg = colors.cyan_2 },
+		LspDiagnosticsDefaultInformation = { fg = colors.cyan_2 },
+		LspDiagnosticsDefaultHint = { fg = colors.robin_blue },
 		-- LspDiagnosticsUnderlineError = { fg = colors.red, undercurl = true },
 		-- LspDiagnosticsUnderlineWarning = { fg = colors.yellow, undercurl = true },
 		-- LspDiagnosticsUnderlineInformation = { fg = colors.cyan, undercurl = true },
